8b100b1ea45ed7903ff2430851770a126cf95930
Merlin Schumacher <merlin.schumacher@gmail.com>
2024-07-28 12:34:03 +0200

    fix koki again

--- Scrapers/Koki/KoKIJsonScraper.cs
+++ Scrapers/Koki/KoKIJsonScraper.cs
@@ -1,4 +1,5 @@
-﻿using kinohannover.Data;
+﻿using HtmlAgilityPack;
+using kinohannover.Data;
 using kinohannover.Helpers;
 using kinohannover.Models;
 using kinohannover.Scrapers.Koki;
@@ -8,29 +9,32 @@ using TMDbLib.Client;
 
 namespace kinohannover.Scrapers
 {
-    public partial class KoKICSScraper(KinohannoverContext context, ILogger<KoKICSScraper> logger, TMDbClient tmdbClient) : ScraperBase(context, logger, tmdbClient, KokiCinema.Cinema), IScraper
+    public partial class KoKIJsonScraper(KinohannoverContext context, ILogger<KoKIJsonScraper> logger, TMDbClient tmdbClient) : ScraperBase(context, logger, tmdbClient, KokiCinema.Cinema), IScraper
     {
-        private class EventDetailJson()
-        {
-            public DateTime StartDate { get; set; }
-            public DateTime EndDate { get; set; }
-            public string Name { get; set; }
-        }
+        private const string _eventDetailElementsSelector = ".//span[contains(@class, 'react-ical')]";
 
-        private readonly string _dataUrl = "https://www.hannover.de/Kommunales-Kino/";
-        private readonly string _shopLink = "https://www.hannover.de/Kommunales-Kino/";
-        private readonly string _baseUrl = "https://www.hannover.de/";
         private const string _eventElementSelector = "//div[contains(@class, 'interesting-single__content')]";
-        private const string _eventDetailElementsSelector = "//span[contains(@class, 'react-ical')]";
-        private const string _readMoreSelector = "//a[contains(@class, 'content__read-more')]";
+
+        private const string _readMoreSelector = ".//a[contains(@class, 'content__read-more')]";
+
+        private readonly Uri _baseUrl = new("https://www.hannover.de/");
+
+        private readonly Uri _dataUrl = new("https://www.hannover.de/Kommunales-Kino/api/v2/view/1274220/0/100/line?identifiers=event&sortField=2&sortOrder=1");
+
+        private readonly string _shopLink = "https://www.hannover.de/Kommunales-Kino/";
+
         private readonly Regex _titleRegex = TitleRegex();
+
         public bool ReliableMetadata => false;
 
         public async Task ScrapeAsync()
         {
-            var doc = await HttpHelper.GetHtmlDocumentAsync(new Uri(_dataUrl));
-
-            var eventElements = doc.DocumentNode.SelectNodes(_eventElementSelector);
+            var eventHtml = await GetEventElements();
+            if (eventHtml is null)
+            {
+                return;
+            }
+            var eventElements = eventHtml.DocumentNode.SelectNodes(_eventElementSelector);
             foreach (var eventElement in eventElements)
             {
                 var eventDetailElement = eventElement.SelectSingleNode(_eventDetailElementsSelector);
@@ -54,7 +58,7 @@ namespace kinohannover.Scrapers
                 }
                 else
                 {
-                    readMoreUrlString = new Uri(new Uri(_baseUrl), readMoreUrlString).ToString();
+                    readMoreUrlString = new Uri(_baseUrl, readMoreUrlString).ToString();
                 }
 
                 var movieTitle = _titleRegex.Match(eventJson.Name).Groups[1].Value;
@@ -83,5 +87,40 @@ namespace kinohannover.Scrapers
 
         [GeneratedRegex(@"\d{1,2}.\d{2}\s*Uhr:\s*(.*)")]
         private static partial Regex TitleRegex();
+
+        private async Task<HtmlDocument?> GetEventElements()
+        {
+            var eventHtmlJson = await HttpHelper.GetJsonAsync<EventHtmlJson>(_dataUrl);
+
+            if (eventHtmlJson?.Success == true)
+            {
+                var eventHtml = string.Concat(eventHtmlJson.Items);
+                var doc = new HtmlDocument();
+                doc.LoadHtml(eventHtml);
+                return doc;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// The JSON object returned by the event detail API.
+        /// </summary>
+        private class EventDetailJson()
+        {
+            public DateTime EndDate { get; set; }
+            public string Name { get; set; }
+            public DateTime StartDate { get; set; }
+        }
+
+        /// <summary>
+        /// The JSON object returned by the event HTML API.
+        /// </summary>
+        private sealed class EventHtmlJson
+        {
+            public required string[] Items { get; set; } = [];
+            public bool Success { get; set; }
+            public required int TotalItems { get; set; } = 0;
+        }
     }
 }
