23b0683346c014c55488930e542afeecf0a961b8
Merlin Schumacher <merlin.schumacher@gmail.com>
2024-09-04 23:32:45 +0200

    swipe fixes

--- frontend/src/components/swiper/swiper.component.ts
+++ frontend/src/components/swiper/swiper.component.ts
@@ -3,6 +3,8 @@ import css from "./swiper.component.css?inline";
 import noContentHtml from "../no-content/no-content.component.html?raw";
 import { ScrollSnapDraggable, ScrollSnapSlider } from "scroll-snap-slider";
 import '../../extensions/ShadowRootExtensions';
+import ChevronBackward from "@material-symbols/svg-400/outlined/chevron_backward.svg?raw";
+import ChevronForward from "@material-symbols/svg-400/outlined/chevron_forward.svg?raw";
 
 const style = new CSSStyleSheet();
 style.replaceSync(css);
@@ -16,13 +18,15 @@ export default class Swiper extends HTMLElement {
   private scrollSnapSlider: ScrollSnapSlider;
   private scrollSnapSliderEl: HTMLElement;
   private triggeredScrollThreshold = false;
+  private shadow: ShadowRoot;
+  private slideCount = 1;
 
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: "open" });
-    shadow.appendChild(template.content.cloneNode(true));
-    shadow.adoptedStyleSheets = [style];
-    this.scrollSnapSliderEl = shadow.safeQuerySelector(".scroll-snap-slider");
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow.appendChild(template.content.cloneNode(true));
+    this.shadow.adoptedStyleSheets = [style];
+    this.scrollSnapSliderEl = this.shadow.safeQuerySelector(".scroll-snap-slider");
     this.scrollSnapSlider = new ScrollSnapSlider({
       element: this.scrollSnapSliderEl,
       sizingMethod(slider) {
@@ -38,6 +42,8 @@ export default class Swiper extends HTMLElement {
 
   connectedCallback() {
     this.removeAllSlides();
+    this.shadow.safeQuerySelector("#swipe-left").innerHTML = ChevronBackward;
+    this.shadow.safeQuerySelector("#swipe-right").innerHTML = ChevronForward;
 
     // Detect if the user has swiped to the last page and load more data
     this.scrollSnapSliderEl.addEventListener("scroll", () => {
@@ -49,6 +55,24 @@ export default class Swiper extends HTMLElement {
         this.dispatchEvent(new CustomEvent("scroll-threshold-reached", { bubbles: true }));
       }
     });
+
+    this.shadowRoot?.safeQuerySelector("#swipe-left").addEventListener("click", () => {
+      console.log("swipe left");
+      const targetSlide = this.scrollSnapSlider.slide - 1
+      if (targetSlide < 0) {
+        return;
+      }
+      this.scrollSnapSlider.slideTo(targetSlide);
+    });
+
+    this.shadowRoot?.safeQuerySelector("#swipe-right").addEventListener("click", () => {
+      console.log("swipe right");
+      const targetSlide = this.scrollSnapSlider.slide + 1
+      if (targetSlide >= this.slideCount) {
+        return;
+      }
+      this.scrollSnapSlider.slideTo(targetSlide);
+    });
   }
 
   public appendSlide(slide: HTMLElement): void {
@@ -56,14 +80,17 @@ export default class Swiper extends HTMLElement {
     slide.classList.add("scroll-snap-slide");
     this.scrollSnapSliderEl.appendChild(slide);
     this.triggeredScrollThreshold = false;
+    this.slideCount++;
   }
 
   public removeAllSlides(): void {
     this.scrollSnapSliderEl.replaceChildren();
+    this.slideCount = 0;
   }
 
   public displayNoResults(): void {
     this.scrollSnapSliderEl.replaceChildren(noResultsElement);
+    this.slideCount = 0;
   }
 
   public replaceSlides(slides: HTMLElement[]): void {
@@ -73,6 +100,7 @@ export default class Swiper extends HTMLElement {
     });
     this.scrollSnapSliderEl.replaceChildren(...slides);
     this.scrollSnapSlider.slideTo(0);
+    this.slideCount = slides.length;
   }
 
   public static BuildElement(): Swiper {
