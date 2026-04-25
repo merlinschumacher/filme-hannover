c2493ce9a73cdd6de24caebc989d5e4ea228001a
Merlin Schumacher <merlin.schumacher@holistic-net.de>
2026-04-25 21:51:41 +0200

    fix a few warnings

--- backend/Renderer/JsonRenderer/JsonDataRenderer.cs
+++ backend/Renderer/JsonRenderer/JsonDataRenderer.cs
@@ -78,7 +78,7 @@ public class JsonDataRenderer(DatabaseContext context) : IRenderer
 				Id = m.Id,
 				DisplayName = m.DisplayName,
 				ReleaseDate = m.ReleaseDate,
-				Cinemas = m.Cinemas.Select(c => c.Id).ToList(),
+				Cinemas = m.Cinemas.ConvertAll(c => c.Id),
 				Runtime = m.Runtime.TotalMinutes,
 				Rating = m.Rating,
 			})
